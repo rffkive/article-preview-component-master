354cf3fc5d65e30b0e714ec41b62d75208238a44
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-07-13 16:06:48 +0800

    try the share button function with console log
    make sure that the first not overlap with the second one

---
+++ script.js
@@ -0,0 +1,14 @@
+const shareBtn = document.querySelector("#shareBtn");
+const shareBtn2 = document.querySelector("#shareBtn2");
+
+shareBtn.addEventListener("click", () => 
+{
+  console.log ("first share button");
+}
+);
+
+shareBtn2.addEventListener("click", () => 
+  {
+    console.log ("second share button");
+  }
+  );
